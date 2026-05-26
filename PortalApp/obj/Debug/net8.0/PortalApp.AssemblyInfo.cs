d76e7b2ce97fe518c17911a1dbab8c304d86036b
Leart Demaku <leart.demaku2006@gmail.com>
2026-05-26 22:28:01 +0200

    Rregullim i UI

--- PortalApp/obj/Debug/net8.0/PortalApp.AssemblyInfo.cs
+++ PortalApp/obj/Debug/net8.0/PortalApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af1dd8770c2bce087f942eb76dd3a26ea4542a50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fbd35b9f359aec6ad3fe5d3c248e68575da2369")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
