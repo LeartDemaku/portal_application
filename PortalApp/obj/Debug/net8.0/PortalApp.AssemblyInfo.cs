c5defbeb21e7a6fcc3e3bf8fbd8040f2f06f72a4
Leart Demaku <leart.demaku2006@gmail.com>
2026-07-31 13:02:03 +0200

    Updated Input

--- PortalApp/obj/Debug/net8.0/PortalApp.AssemblyInfo.cs
+++ PortalApp/obj/Debug/net8.0/PortalApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189521023f4b8f24d656595d1256002fc7be259c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2c023a54c12ae6cb6e392f8cff5de7c0898981")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
